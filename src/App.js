472d5a70942c3591c8592f356fc78b5bb20c3296
Naveen <sunkaranaveensunkara@gmail.com>
2021-11-24 09:49:16 +0530

    created home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-
+import React from "react"
+import {Link,Route} from 'react-router-dom'
+import Home from "./Home"
 const App=(props)=>{
     return(
          <div>
-             
+             <Link to='/'>Home</Link>|
+             <Link to='/student'>Student</Link>|
+             <Link to='/admin'>Admin</Link>|
+
+             <Route path='/' component={Home} exaxt={true}></Route>
          </div>
     )
 }
